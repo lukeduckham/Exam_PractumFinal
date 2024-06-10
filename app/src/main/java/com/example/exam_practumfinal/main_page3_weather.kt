904bac8f16cb271f68b97e26f347fde55e5f3b9b
lukeduckham <ST10440199@vcconnect.edu.za>
2024-06-10 11:14:17 +0200

    Commit 3 for added code

--- app/src/main/java/com/example/exam_practumfinal/main_page3_weather.kt
+++ app/src/main/java/com/example/exam_practumfinal/main_page3_weather.kt
@@ -2,15 +2,14 @@ package com.example.exam_practumfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.time.DayOfWeek
 
-class MainPage2_weather : AppCompatActivity() {
+class main_page3_weather : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_page2_weather)
+        setContentView(R.layout.activity_main_page3_weather)
 
         fun main(args: Array<String>) {
-        val DayOfWeek = arrayOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+            val DayOfWeek = arrayOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
 
 
@@ -21,5 +20,7 @@ class MainPage2_weather : AppCompatActivity() {
                     i--
                 }while(i > 0)
             }
+
+
     }
 }}
