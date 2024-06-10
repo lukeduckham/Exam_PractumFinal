a6d4bb8f71eec74128d06c764143f3666a4e7fbf
lukeduckham <ST10440199@vcconnect.edu.za>
2024-06-10 10:14:56 +0200

    Commit 2 for added code

--- app/src/main/java/com/example/exam_practumfinal/MainActivity.kt
+++ app/src/main/java/com/example/exam_practumfinal/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.exam_practumfinal
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,11 +18,31 @@ class MainActivity : AppCompatActivity() {
             val Intent = Intent(this,MainPage2_weather::class.java)
             startActivity(Intent)
 
-
+// declared my continue button and put in the code for it to go onto the next page
 
         val ExitButton = findViewById<Button>(R.id.ExitButton)
             ExitButton.setOnClickListener {
                 val Intent = Intent(this,MainActivity::class.java)
                 startActivity(Intent)
-    }
-}}}
+
+ // declared my exit button and put in the code for it to go onto the next page
+
+                ContinueButton?.setOnClickListener {
+                    Toast.makeText(this@MainActivity,
+                        "Feed Button clicked", Toast.LENGTH_SHORT).show()
+
+                     ExitButton?.setOnClickListener {
+                        Toast.makeText(this@MainActivity,
+                            "Feed Button clicked", Toast.LENGTH_SHORT).show()
+
+//this code is for the indication that the button has been clicked
+
+                    }}}}}}
+
+/* References:
+
+The Independent Institution of Education. (2024). Introduction to Mobile Application Development. IMAD5112. Varsity College Pietermaritzburg.
+[online] Available at: Tertiary Students - IMAD5112MM.pdf - Default View (sharepoint.com).[Accessed : 1 May 2024]
+/*
+
+ */
--- app/src/main/java/com/example/exam_practumfinal/MainPage2_weather.kt
+++ app/src/main/java/com/example/exam_practumfinal/MainPage2_weather.kt
@@ -2,10 +2,19 @@ package com.example.exam_practumfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.time.DayOfWeek
 
 class MainPage2_weather : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page2_weather)
+
+        fun main(args: Array<String>) {
+        val DayOfWeek = arrayOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+
+
+
+
     }
-}
+}}
